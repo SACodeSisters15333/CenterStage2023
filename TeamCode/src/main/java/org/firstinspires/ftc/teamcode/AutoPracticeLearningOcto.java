40b201418c0e58238e3976ee35ca7e9b0dcd3314
kristina.sorrells <kristina.sorrells@salem.edu>
2023-10-06 14:55:21 -0400

    Learning about a hardware map

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPracticeLearningOcto.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+//REMINDER -- Battery Level effects Motor Power which effects timing
+
+@Autonomous (name = "MecPlayAuto")
+
+@Disabled
+
+public class AutoPracticeLearningOcto extends LinearOpMode {
+
+    HardwareMapCenterStage15333 Wall_E = new HardwareMapCenterStage15333();
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+
+        System.out.println("Starting up");
+        telemetry.addData("init pressed", "about to initialize");
+        telemetry.update();
+
+        System.out.println("Initialize Robot");
+        Wall_E.InitializeRobot(hardwareMap);
+        System.out.println("Robot Initialized");
+
+        telemetry.addData("Status", "Ready!");
+
+        telemetry.update();
+
+        waitForStart();
+
+        Wall_E.DriveStraight(.5, 500, 1); //Forward
+        Thread.sleep(1000);
+        Wall_E.DriveSideways(1,1000,1); //Drive Right
+        Thread.sleep(500);
+        Wall_E.DriveSideways(1,1500,-1); //Drive Right
+        Thread.sleep(500);
+        Wall_E.DiagonalForward(.5, 1000, -1); //Backward
+        Thread.sleep(250);
+
+    }
+}
